ce7cb9ac9dd2b55dc89f639e9d493094551c686f
mortvicious <shindo.shitai@gmail.com>
2022-08-01 12:13:58 +0400

    structure refactor

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+console.log(`4ggg534`);
---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
+
+console.log(`4534`)
